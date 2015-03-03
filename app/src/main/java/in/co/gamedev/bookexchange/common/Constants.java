450e58d344b9294e758d9086e3ca60ea600bccba
suhas <suphya4u@gmail.com>
2015-03-02 17:06:06 -0800

    Changing appenging app to book-exchanger

--- app/src/main/java/in/co/gamedev/bookexchange/common/Constants.java
+++ app/src/main/java/in/co/gamedev/bookexchange/common/Constants.java
@@ -11,14 +11,13 @@ public class Constants {
 
   public static final String INTENT_EXTRA_SELECTED_BOOK = "intent_extra_selected_book";
 
-  public static final String SENDER_ID = "1004021566470";
-
   public static final String INTENT_EXTRA_LIST_TYPE = "BOOK_LIST_TYPE";
   public static final String BOOK_LIST_TYPE_MY_BOOKS = "MY_BOOKS";
   public static final String BOOK_LIST_TYPE_EXPECTED_BOOKS = "EXPECTED_BOOKS";
   public static final String APPROVAL_STATUS_APPROVED = "APPROVED";
 
-  public static final String SERVER_URL = "https://suphya4u.appspot.com/_ah/api/";
+  public static final String SERVER_URL = "https://book-exchanger.appspot.com/_ah/api/";
+  public static final String SENDER_ID = "863338352615";
   public static final boolean IS_LOCAL = true;
   public static final String LOCAL_SERVER = "http://10.0.2.2:8080/_ah/api/";
 
